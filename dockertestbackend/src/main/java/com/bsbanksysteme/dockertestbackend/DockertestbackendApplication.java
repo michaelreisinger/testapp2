fe3fb9142e6ed1f476006a1e9c232ab5d50aef61
fhs43050 <mreisinger.itsb-b2018@fh-salzburg.ac.at>
2021-08-17 18:56:13 +0200

    backend app for testapp2

---
+++ dockertestbackend/src/main/java/com/bsbanksysteme/dockertestbackend/DockertestbackendApplication.java
@@ -0,0 +1,13 @@
+package com.bsbanksysteme.dockertestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertestbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertestbackendApplication.class, args);
+	}
+
+}
---
+++ dockertestbackend/src/test/java/com/bsbanksysteme/dockertestbackend/DockertestbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsbanksysteme.dockertestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertestbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
